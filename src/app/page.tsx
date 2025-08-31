31ffee8769837056b99b16e893b707160fbc1faa
AnasAmchaar <anass.amchaar14@gmail.com>
2025-08-31 23:22:19 +0100

    Refactor image source paths in Home component to use imported asset for improved maintainability and consistency.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import fatalErrorLogo from "../../public/images/fatal error logo .png";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
             <div className="flex-1">
               <Link href="/" className="inline-flex items-center space-x-4">
                 <Image
-                  src={"/images/fatal%20error%20logo%20.png"}
+                  src={fatalErrorLogo}
                   alt="Fatal Error Logo"
                   width={128}
                   height={128}
@@ -52,7 +53,7 @@ export default function Home() {
         {/* Background Logo */}
         <div className="absolute inset-0 flex items-center justify-center opacity-5">
           <Image
-            src={"/images/fatal%20error%20logo%20.png"}
+            src={fatalErrorLogo}
             alt="Fatal Error Logo Background"
             width={1000}
             height={1000}
@@ -66,7 +67,7 @@ export default function Home() {
             {/* Large Logo Display */}
             <div className="mb-10">
                         <Image
-                src={"/images/fatal%20error%20logo%20.png"}
+                src={fatalErrorLogo}
                 alt="Fatal Error Logo"
                 width={260}
                 height={260}
@@ -111,7 +112,7 @@ export default function Home() {
             <div className="grid md:grid-cols-5 gap-8 md:gap-12 items-center">
               <div className="md:col-span-2 flex items-center justify-center">
                 <Image
-                  src={"/images/fatal%20error%20logo%20.png"}
+                  src={fatalErrorLogo}
                   alt="Fatal Error Logo"
                   width={360}
                   height={360}
